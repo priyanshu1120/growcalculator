317175ce56e245d6e926d193da4d9f8f9074c771
priyanshu1120 <priyanshukaushik796@gmail.com>
2023-01-21 13:56:11 +0530

    calulator done

--- routes/calculate.route.js
+++ routes/calculate.route.js
@@ -3,13 +3,9 @@ const calculateRouter = express.Router();
 
 calculateRouter.post("/",(req,res)=>{
     let {amount,rate,year} = req.body
-
     ratepercent = rate/100
     Maturity  =  amount * [(((1+ratepercent )**year)-1)/ratepercent ]
-    Total_Investment_Amount = amount * year
-    Total_Interest_Gained  =  Maturity - Total_Investment_Amount
-    console.log(Total_Interest_Gained )
-//    res.status(200).send(Total_Interest_Gained )
+    res.send({MaturityValue:Math.floor(Maturity),InvestAmount:amount*year,netInterest:Math.floor(Maturity-(amount*year))})
 })
 
 module.exports = {calculateRouter}
